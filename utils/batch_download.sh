14c0100657f91976207ee33a5927d902961205c0
Ed Summers <ehs@pobox.com>
2019-03-29 11:53:54 -0400

    cleaned up utils

--- utils/batch_download.sh
+++ utils/batch_download.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-#sbatch -A tr5fp9p -t 48:00:00 -N 1 -C EGRESS -p RM-shared download.sh
+# used to download the content using XSEDE's globus setup
+
 sbatch -t 48:00:00 -N 1 -C EGRESS -p RM-shared download.sh
