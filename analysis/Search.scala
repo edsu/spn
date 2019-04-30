0c9e174080aca68522f8dd0c858e66e20f34d501
Ed Summers <ehs@pobox.com>
2019-04-30 16:15:51 -0400

    got url matching working

--- analysis/Search.scala
+++ analysis/Search.scala
@@ -1,28 +1,23 @@
 import scala.util.matching.Regex
 
-//import de.l3s.archivespark._
-//import de.l3s.archivespark.implicits._
-//import de.l3s.archivespark.enrich.functions._
-//import de.l3s.archivespark.specific.warc.implicits._
-//import de.l3s.archivespark.specific.warc.specs._
-//import de.l3s.archivespark.specific.warc.enrichfunctions._
+import de.l3s.archivespark._
+import de.l3s.archivespark.implicits._
+import de.l3s.archivespark.enrich.functions._
+import de.l3s.archivespark.specific.warc.implicits._
+import de.l3s.archivespark.specific.warc.specs._
+import de.l3s.archivespark.specific.warc.enrichfunctions._
 
-//val cdxPath = "/Users/ed/Projects/spn/spn/*/*.cdx.gz"
-//val warcPath = "/Users/ed/Projects/spn/spn/"
-//val rdd = ArchiveSpark.load(WarcCdxHdfsSpec(cdxPath, warcPath))
-
-
-val urlMatch(pattern: String, url: String) : Boolean => {
-    //val match = pattern.r findFirstIn url
-    val m = "foo";
-    if (m == None) {
+def urlMatch(pattern: String, url: String) : Boolean = {
+    val m = pattern.r findFirstIn url
+    if (m != None) {
         return true
     } else {
         return false
     }
 }
 
-println(urlMatch("geocities", "https://geocities.com"))
+val cdxPath = "/Users/edsu/Data/spn/*/*.cdx.gz"
+val warcPath = "/Users/edsu/Data/spn"
+val rdd = ArchiveSpark.load(WarcCdxHdfsSpec(cdxPath, warcPath))
 
-
-
+var filtered = rdd.filter(r => urlMatch("geocities", r.originalUrl))
