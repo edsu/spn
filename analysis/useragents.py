dde438dd6b0881e3a0b840cce1fdd77cc42ced28
Ed Summers <ehs@pobox.com>
2019-05-26 10:09:55 -0400

    created script

--- analysis/useragents.py
+++ analysis/useragents.py
@@ -22,25 +22,48 @@ def ua(rec):
     if rec.rec_type == 'request':
         ua = rec.http_headers.get('user-agent')
         if ua:
-            yield (ua,)
+            yield ua
 
 
-# We're going to examine them year by year.
+# Let's create a function that will run a Spark job to get the User-Agent counts for a year as a dictionary.
+
+# In[22]:
+
+
+import pandas
+
+def get_year(year):
+    warc_files = glob.glob('warcs/liveweb-{}*/*.warc.gz'.format(year))
+    warcs = sc.parallelize(warc_files)
+    output = warcs.mapPartitions(ua)
+    return output.countByValue()
+
+
+# Now we can use `get_year` for each year of data we have for SPN and create a pandas DataFrame of the results.
 
 # In[ ]:
 
 
-from glob import glob
-
+ua_data = {}
 for year in range(2013, 2019):
-    warc_files = glob("warcs/liveweb-{}*/*warc.gz".format(year))
-    if len(warc_files) == 0:
-        continue
-    warcs = sc.parallelize(warc_files)
+    print(year)
+    ua_data[str(year)] = get_year(year)
 
-    output = warcs.mapPartitions(ua)
-    df = output.toDF(["ua"])
-    df.write.csv("out/ua-{}".format(year))    
+df = pandas.DataFrame(ua_data)
+df.index.name = 'ua'
+
+
+# In[ ]:
+
+
+df = df.sort_values(by='2018', ascending=False)
+print(df.head())
+
+
+# In[ ]:
+
+
+df.to_csv('../analysis/results/ua.csv')
 
 
 # In[ ]:
